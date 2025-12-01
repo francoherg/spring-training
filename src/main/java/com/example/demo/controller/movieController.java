5c802fdeeb839336697b5b8f0b1a0dd11a5b5171
Franco <francoherg@gmail.com>
2025-12-01 17:57:48 -0300

    movies devuelve listado

--- src/main/java/com/example/demo/controller/movieController.java
+++ src/main/java/com/example/demo/controller/movieController.java
@@ -2,12 +2,18 @@ package com.example.demo.controller;
 
 import com.example.demo.entities.Movie;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
 public class movieController {
     @GetMapping("/movies")
-    public Movie movies() {
-        return new Movie("Pepito pepon once again 2: mas pepeado que nunca", 240, "Messi");
+    public Movie[] movies() { // tambien se puede usar list
+        return new Movie[] {
+                new Movie("Pepito pepon once again 2: mas pepeado que nunca", 240, "Messi")
+        };
     }
+
 }
