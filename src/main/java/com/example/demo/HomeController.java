d7ce3d40a8add102f704093873a6b2f2cf90f94f
Franco <francoherg@gmail.com>
2025-11-30 21:19:02 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    public record Usuario(String nombre) {}
+
+    @GetMapping("/api/usuario")
+    public Usuario usuario() {
+        return new Usuario("pepito pepon");
+    }
+}
